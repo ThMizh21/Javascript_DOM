a490f522f368f1052c7ccf8a38991f6c8fe25606
ThMizh21 <muthamizhanboobalan@gmail.com>
2024-10-10 10:45:32 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,33 @@
+
+function color() {
+  const colour =document.getElementById("header");
+
+ if(colour.style.background===""){
+     colour.style.background="black"
+ }
+ else{
+     colour.style.background=""
+ }
+ 
+}
+setInterval(color,3000)
+
+const container = document.getElementById("header");
+const butto = document.getElementById("c");
+const butt =document.getElementById("d")
+
+butto.addEventListener("click", () => {
+    const newPar = document.createElement("p");
+    newPar.textContent= "This is the third paragraph";
+    container.appendChild(newPar);
+
+});
+
+butt.addEventListener("click", () => {
+    const newPar = document.createElement("p");
+    newPar.textContent= "This is the third paragraph";
+    container.appendChild(newPar);
+
+});
+
+
