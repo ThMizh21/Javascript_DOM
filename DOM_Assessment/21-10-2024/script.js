e14eefb7a4d8ee1c148631f9afb6e921a34a091f
ThMizh21 <muthamizhanboobalan@gmail.com>
2024-10-21 11:55:22 +0530

    .

--- DOM_Assessment/21-10-2024/script.js
+++ DOM_Assessment/21-10-2024/script.js
@@ -11,10 +11,7 @@ form.addEventListener("submit", (event)=>{
  ageError.textContent = "";
  salaryError.textContent = "";
   if(nam.validity.valid){
-    if(!nam.validity.valueMissing){
-        nameError.textContent = "Enter a valid employee name!";
-    }
-    else if(!(nam.value.length>=3 && nam.value.length <=30) ){
+ if(!(nam.value.length>=3 && nam.value.length <=30) ){
         nameError.textContent = "Name must be between 3 and 30 characters.";
     }
     }
